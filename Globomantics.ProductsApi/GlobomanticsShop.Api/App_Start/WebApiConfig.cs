d51a03e55d612a1895be997462f2e2aff6784291
Nertil Poci <nertilpoci@hotmail.com>
2019-11-22 10:08:04 +0100

    Enable CORS Globally

--- Globomantics.ProductsApi/GlobomanticsShop.Api/App_Start/WebApiConfig.cs
+++ Globomantics.ProductsApi/GlobomanticsShop.Api/App_Start/WebApiConfig.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace GlobomanticsShop.Api
 {
@@ -13,6 +15,16 @@ namespace GlobomanticsShop.Api
             // Web API configuration and services
             config.Formatters.JsonFormatter.SupportedMediaTypes
     .Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+            //cors configuration
+
+            var cors = new EnableCorsAttribute("http://google.com", "*", "*");
+            config.EnableCors(cors);
+
+
+            //--end of cors configuration
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- Globomantics.ProductsApi/GlobomanticsShop.Api/Global.asax.cs
+++ Globomantics.ProductsApi/GlobomanticsShop.Api/Global.asax.cs
@@ -13,6 +13,7 @@ namespace GlobomanticsShop.Api
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
